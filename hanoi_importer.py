80431740539c3ee1842c0595d2ec210a299b02ec
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-06-15 22:32:57 +0700

    init code

---
+++ hanoi_importer.py
@@ -0,0 +1,31 @@
+# Import necessary libraries
+from datetime import datetime
+import pandas as pd
+from meteostat import Point, Daily
+import os
+
+# Set time period
+start = datetime(2018, 1, 1)
+end = datetime(2023, 12, 31)
+
+location = Point(21.0245, 105.8412)
+
+# Get daily data for 2018
+data = Daily(location, start, end)
+data = data.fetch()
+
+# Convert fetched data to pandas DataFrame
+df = data.interpolate()  # Interpolate missing values if any
+
+# Export data to Excel
+excel_file = 'weather_data_hanoi.xlsx'
+
+# Delete the file if it exists
+if os.path.exists(excel_file):
+    os.remove(excel_file)
+    print(f"Old {excel_file} deleted.")
+    
+df.to_excel(excel_file, index=False)
+
+print(f"Data has been exported to {excel_file}")
+print(data)
---
+++ main.py
@@ -0,0 +1,2 @@
+import hanoi_importer
+import hcm_importer
